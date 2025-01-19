7a0d720ab51abd7500510e7f37fd4aed35cb16ee
kdukuray <kdukuray01@gmail.com>
2025-01-19 16:56:25 -0500

    added logic for set and add operations (not tested)

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,76 @@ enum Token{
     If,
     Print,
     Jump,
-    R(i32),
+    R(usize),
+    Val(i32),
     #[default]
     None
 
 }
+fn parse_string_to_int(string: &str) -> i32{
+    string.trim().parse::<i32>().unwrap()
+}
 
 // This function takes in an instruction,
 //does the necessary lexical analysis and executes it
-fn execute_instruction(instruction: &str){
+fn execute_instruction(registers: &mut Vec<i32>, instruction: &str){
     let tokenized_line: Vec<Token> = tokenize_line(instruction);
+    match tokenized_line[0]{
+        Token::Add => {
+            let mut operand_1_index: usize = Default::default();
+            let mut operand_2_index: usize = Default::default();
+
+            match tokenized_line[1]{
+                Token::R(index) => operand_1_index = index,
+                _ => println!("Error!")
+            }
+            match tokenized_line[2]{
+                Token::R(index) => operand_2_index = index,
+                _ => println!("Error!")
+            }
+
+            registers[operand_1_index] = registers[operand_1_index] + registers[operand_2_index];
+
+        }
+        Token::Sub => {
+
+        }
+        Token::Mul => {
+
+        }
+        Token::Div => {
+
+        }
+        Token::Set => {
+            let mut operand_1_index: usize = Default::default();
+            let mut value: i32 = Default::default();
+
+            match tokenized_line[1]{
+                Token::R(index) => operand_1_index = index,
+                _ => println!("Error!")
+            }
+
+            match tokenized_line[2]{
+                Token::Val(value_) => value = value_,
+                _ => println!("Error!")
+            }
+
+            registers[operand_1_index] = value;
+
+        }
+        // How do I execute if statements?
+        Token::If => {
+
+        }
+        Token::Print => {
+
+        }
+        // How do I implement jump statements?
+        Token::Jump => {
+
+        }
+        _ => println!("Error!")
+    }
     println!("Tokenized Line {:?}", tokenized_line);
 }
 
@@ -54,7 +114,7 @@ fn tokenize(fragment: &str) -> Token{
         token = Token::Jump;
     }
     else if fragment.to_lowercase().starts_with("r"){
-        let register_number: i32 = fragment[1..].parse::<i32>().unwrap();
+        let register_number: usize = fragment[1..].parse::<usize>().unwrap();
         token = Token::R(register_number);
     }
     else{
@@ -76,6 +136,10 @@ fn tokenize_line(instruction:&str) -> Vec<Token>{
     tokenized_instruction
 }
 
+fn print_registers(registers: &Vec<i32>){
+
+}
+
 fn main(){
 
     // Getting a reference to the file
@@ -84,9 +148,12 @@ fn main(){
 
     let file_reader_buffer: BufReader<File> = BufReader::new(file_reference);
 
+    // Register logic
+    let mut registers: Vec<i32> = Vec::new();
+
     // loop through every line, and execute the code
     for instruction in file_reader_buffer.lines(){
-        execute_instruction(&instruction.unwrap());
+        execute_instruction(&mut registers, &instruction.unwrap());
     }
 
 }
