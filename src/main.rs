1ce7e0114a2ae0cc6a0ac1324b1c4ff57c6d2733
kdukuray <kdukuray01@gmail.com>
2025-01-20 21:49:45 -0500

    more documentation and minor code changes

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,15 @@ enum Token{
     None
 
 }
+
+// This functions takes in an integer as a string and
+// returns the integer representation of the string
 fn parse_string_to_int(string: &str) -> i32{
     string.trim().parse::<i32>().unwrap()
 }
+
+// This function takes in a register token and
+// return the index associated with the register
 fn get_register_index(token: &Token) -> usize{
     let mut register_index: usize = Default::default();
     match token{
@@ -29,6 +35,8 @@ fn get_register_index(token: &Token) -> usize{
     register_index
 }
 
+// This function takes in a value token and
+// return the actual numeric value associated with the value
 fn get_value_value(token: &Token) -> i32{
     let mut value:i32 = 0;
     match token{
@@ -42,9 +50,9 @@ fn get_value_value(token: &Token) -> i32{
 fn execute_instruction(registers: &mut Vec<i32>, instruction: &str) -> i32{
     let tokenized_line: Vec<Token> = tokenize_line(instruction);
     let mut exit_code: i32 = 0;
-    // if exit code is 0, do nothing
+    // if exit code is 0, do nothing ie, go to the next line of code
     // if exit_code is a positive number, jump to that line
-    // if exit code is -1, if statement condition failed, jump the next line
+    // if exit code is -1, an if statement condition failed, skip the next line
     match tokenized_line[0]{
         Token::Add => {
             let operand_1_index: usize = get_register_index(&tokenized_line[1]);
@@ -73,11 +81,9 @@ fn execute_instruction(registers: &mut Vec<i32>, instruction: &str) -> i32{
         Token::Set => {
             let operand_1_index: usize = get_register_index(&tokenized_line[1]);
             let value: i32 = get_value_value(&tokenized_line[2]);
-
             registers[operand_1_index] = value;
 
         }
-        // How do I execute if statements?
         Token::If =>{
             let operand_1_index: usize = get_register_index(&tokenized_line[1]);
             let value: i32 = get_value_value(&tokenized_line[2]);
@@ -155,25 +161,24 @@ fn tokenize_line(instruction:&str) -> Vec<Token>{
     tokenized_instruction
 }
 
+//This function prints the registers
 fn print_registers(registers: &Vec<i32>){
     println!("{:?}", registers);
 
 }
 
 fn main(){
-
-    // Getting a reference to the file
+    // Getting a reference to the file and loading it to a buffer
     let file_path: String = String::from("C:\\Users\\kduku\\RustroverProjects\\learning_rust\\src\\asmmm.txt");
     let file_reference: File = File::open(file_path).unwrap();
-
     let file_reader_buffer: BufReader<File> = BufReader::new(file_reference);
 
-    // Register logic
-    let mut registers: Vec<i32> = [0, 0, 0, 0, 0, 0, 0, 0, 0].to_vec();
+    // Register logic (We will only have 9 registers
+    let mut registers: Vec<i32> = Vec::from([0; 10]);
 
     // loop through every line, and execute the code
-
     let mut line_index: i32 = 0;
+    // exit_code represents the status the of the most recently executed line of code
     let mut exit_code: i32 = 0;
     let mut code_base: Vec<String> = Default::default();
     for line_of_code in file_reader_buffer.lines(){
@@ -194,5 +199,6 @@ fn main(){
             line_index += 2;
         }
     }
+    println!("Program was executed successfully");
 
 }
